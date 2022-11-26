bbe6064a75a636b3e2ce8fd42a53e38613627267
Sumit Barua <sumitbarua14@gmail.com>
2022-11-26 15:36:59 +0600

    day1

---
+++ Javascript/access-array-data-with-indexes.js
@@ -0,0 +1,4 @@
+const myArray = [50, 60, 70];
+//Create a variable called myData and set it to equal the first value of myArray using bracket notation.
+const myData=myArray[0]
+
---
+++ Javascript/nest-one-array-within-another-array.js
@@ -0,0 +1,3 @@
+// Only change code below this line
+const myArray = [["sumit",22],["Sumit",26]];
+console.log(myArray)
