c6b0fc973210c533aac4a51f3f8dd4ebf58a88ca
Sumit Barua <sumitbarua14@gmail.com>
2022-12-13 00:56:09 +0600

    day3

---
+++ Javascript/22.local-scope-and-functions.js
@@ -0,0 +1,9 @@
+function myLocalScope() {
+  // Only change code below this line
+   const myVar ="Sumit"
+  console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
