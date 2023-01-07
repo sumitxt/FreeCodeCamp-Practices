134a8f71b4781619a8d4a25607a35518bc99bf69
sumitxt <sumitbarua14@gmail.com>
2023-01-07 14:25:08 +0600

    day7

---
+++ Javascript/52.build-javascript-objects.js
@@ -0,0 +1,11 @@
+const myDog = {
+  // Only change code below this line
+
+  "name":"Sumit",
+  "legs":2,
+  "tails":1,
+  "friends":["Rudro","Tonmoy","Ridwan","Antu"]
+
+
+  // Only change code above this line
+};
