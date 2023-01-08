80ef5c91cf7ed17b28d6ac24cd8ce9728d14b5ba
sumitxt <sumitbarua14@gmail.com>
2023-01-08 14:12:17 +0600

    day8

---
+++ Javascript/61.manipulating-complex-objects.js
@@ -0,0 +1,25 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist":"Sumit Barua",
+    "title":"JavaScript pare na",
+    "release_year":2017,
+    "formats":[
+      "CD",
+      "8T",
+      "LP",
+      "MP3",
+      "MV"
+    ]
+  }
+];
